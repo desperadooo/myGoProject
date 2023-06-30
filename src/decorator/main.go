b23a7ffc5fe5ed4afefd0f64319d46fd77e07630
Huangkai Zheng <huangkai.zheng@shopee.com>
2023-06-30 15:48:48 +0800

    Decorator demo

--- src/decorator/main.go
+++ src/decorator/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"reflect"
 )
 
 func WithServerHeader(h http.HandlerFunc) http.HandlerFunc {
@@ -19,10 +20,41 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World! "+r.URL.Path)
 }
 
+func foo(a, b, c int) int {
+	fmt.Printf("%d, %d, %d\n", a, b, c)
+	return a + b + c
+}
+
+func bar(a, b string) string {
+	fmt.Printf("%s, %s\n", a, b)
+	return a + b
+}
+
+func Decorator(decoPtr, fn interface{}) (err error) {
+	var decoratedFunc, targetFunc reflect.Value
+
+	decoratedFunc = reflect.ValueOf(decoPtr).Elem()
+	targetFunc = reflect.ValueOf(fn)
+
+	v := reflect.MakeFunc(targetFunc.Type(),
+		func(in []reflect.Value) (out []reflect.Value) {
+			fmt.Println("before")
+			out = targetFunc.Call(in)
+			fmt.Println("after")
+			return
+		})
+
+	decoratedFunc.Set(v)
+	return
+}
+
 func main() {
-	http.HandleFunc("v1/hello", WithServerHeader(hello))
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	mybar := bar
+	Decorator(&mybar, bar)
+	mybar("hello", "world!")
+	//http.HandleFunc("v1/hello", WithServerHeader(hello))
+	//err := http.ListenAndServe(":8080", nil)
+	//if err != nil {
+	//	log.Fatal("ListenAndServe: ", err)
+	//}
 }
